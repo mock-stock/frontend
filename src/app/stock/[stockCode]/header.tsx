dd975d82e7b49484088a6aac4d46498ec5af8e68
suin <73993670+whl5105@users.noreply.github.com>
2025-01-15 23:34:02 +0900

    test : vercel test

--- src/app/stock/[stockCode]/header.tsx
+++ src/app/stock/[stockCode]/header.tsx
@@ -3,9 +3,13 @@
 import Icons from "@/components/icons";
 import style from "./header.module.scss";
 import { useRouter } from "next/navigation";
+// import { StockData } from "./page";
+import { useStocksInfoSocket } from "./hooks/useStocks";
 
-export default function Header() {
+export default function Header({ stockCode }: { stockCode: string }) {
   const router = useRouter();
+  const { data } = useStocksInfoSocket(stockCode);
+  console.log(data);
   return (
     <header className={style["header-container"]}>
       <div className={style["back-icon-box"]}>
