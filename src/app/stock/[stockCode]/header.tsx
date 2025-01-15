f0980c6c9418d327a68141b5aa169f2dc5d5ed48
suin <73993670+whl5105@users.noreply.github.com>
2025-01-15 23:37:19 +0900

    test : vercel test

--- src/app/stock/[stockCode]/header.tsx
+++ src/app/stock/[stockCode]/header.tsx
@@ -3,12 +3,42 @@
 import Icons from "@/components/icons";
 import style from "./header.module.scss";
 import { useRouter } from "next/navigation";
-// import { StockData } from "./page";
-import { useStocksInfoSocket } from "./hooks/useStocks";
+import { StockData } from "./page";
+// import { useStocksInfoSocket } from "./hooks/useStocks";
+import { useEffect, useState } from "react";
+import { getStock } from "@/api/stock";
 
 export default function Header({ stockCode }: { stockCode: string }) {
   const router = useRouter();
-  const { data } = useStocksInfoSocket(stockCode);
+  const [data, setData] = useState<StockData>();
+  // const { data } = useStocksInfoSocket(stockCode);
+  console.log(data);
+  const fetchData = async () => {
+    const stockData = await getStock(stockCode);
+    setData(stockData);
+  };
+
+  useEffect(() => {
+    if (!stockCode) return;
+
+    // 데이터 패치
+    fetchData();
+
+    // // 소켓 연결
+    // const { unsubscribe } = connectSocket(
+    //   WS_PATHS.SUB_ENDPOINT(stockCode),
+    //   WS_PATHS.PUB_ENDPOINT,
+    //   { action: "SUBSCRIBE", ids: [stockCode] },
+    //   (socketData) => {
+    //     setData(socketData); // 소켓 데이터로 업데이트
+    //   }
+    // );
+
+    // return () => {
+    //   // 구독 취소
+    //   unsubscribe();
+    // };
+  }, [stockCode]);
   console.log(data);
   return (
     <header className={style["header-container"]}>
