818e25c1da64fe84b059e26263e9a9de2bb26a4a
suin <73993670+whl5105@users.noreply.github.com>
2025-02-06 17:08:03 +0900

    fix : useSearchParams hook build error
    - SearchHeader 컴포넌트에서 useSearchParams()를 Suspense 경계로 감싸 오류 해결

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,26 +1,18 @@
-// import { getHistory } from "@/app/api/history";
-// import { getServerToken } from "@/lib/auth";
-// import SearchHistorySection from "./(section)/searchHistorySection";
-
 import style from "./page.module.scss";
 import SearchHeader from "./(section)/searchHeader";
+import { Suspense } from "react";
 export interface HistoryData {
   fid: number;
   search_keyword: string;
 }
 
-// async function fetchHistory() {
-//   const session = await getServerToken();
-//   return session ? await getHistory() : [];
-// }
-
-export default async function Page() {
-  // const data: HistoryData[] = await fetchHistory();
-
+export default function Page() {
   return (
     <div className={style["page-container"]}>
-      <SearchHeader />
-      <main>{/* <SearchHistorySection historyList={data} /> */}</main>
+      <Suspense fallback={<>...Loading</>}>
+        <SearchHeader />
+      </Suspense>
+      <main></main>
     </div>
   );
 }
