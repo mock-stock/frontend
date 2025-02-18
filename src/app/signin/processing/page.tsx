48b74f821fdf47ec112af85fb4c740ac85a3b6ab
suin <73993670+whl5105@users.noreply.github.com>
2025-02-18 20:57:50 +0900

    fix : ESLint build err and 클라이언트 컴포넌트에서 비동기 함수 처리 방식 수정
    - 클라이언트 컴포넌트에 적용된 async 함수 삭제 및 Suspense를 사용하여 토큰 처리 및 리디렉션 처리
    - ESLint: 불필요하게 선언된 코드 주석 처리

--- src/app/signin/processing/page.tsx
+++ src/app/signin/processing/page.tsx
@@ -1,57 +1,49 @@
 "use client";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect } from "react";
 
-export default async function ProgressPage() {
+const sendTokenAndRedirect = async (
+  accessToken: string,
+  redirectUrl: string
+) => {
+  const response = await axios.post(
+    "/api/auth/sendToken",
+    { accessToken },
+    { headers: { "Content-Type": "application/json" } }
+  );
+
+  if (response.data.success) {
+    return redirectUrl;
+  } else {
+    throw new Error(response.data.message || "인증 실패");
+  }
+};
+
+export default function ProgressPage() {
   const searchParams = useSearchParams();
   const router = useRouter();
 
   const token = searchParams.get("token");
   const redirectUrl = searchParams.get("redirect") || "/";
 
-  const [loading, setLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
   useEffect(() => {
-    if (!token || !redirectUrl) {
-      setLoading(false);
-      router.push(redirectUrl);
-      return;
-    }
-    sendTokenAndRedirect(token, redirectUrl);
-  }, [token]);
-
-  const fetcher = (url: string, data: any) =>
-    axios
-      .post(url, data, { headers: { "Content-Type": "application/json" } })
-      .then((res) => res.data)
-      .catch((err) => {
-        throw new Error(err.response?.data?.message || "API 요청 오류");
-      });
-
-  const sendTokenAndRedirect = (accessToken: string, redirectUrl: string) => {
-    setLoading(true);
-
-    fetcher("api/auth/sendToken", { accessToken }).then((result) => {
-      if (result.success) {
-        // 인증 성공 시
-        const encodedRedirectUrl = encodeURIComponent(redirectUrl);
-        router.push(encodedRedirectUrl);
-      } else {
-        setErrorMessage(result.message || "인증 실패");
-        setLoading(false);
+    const fetchData = async () => {
+      if (!token || !redirectUrl) {
+        router.push(redirectUrl);
+        return;
       }
-    });
-  };
 
-  return (
-    <>
-      {loading ? (
-        <p>로그인 처리 중...</p>
-      ) : errorMessage ? (
-        <p>{errorMessage}</p>
-      ) : null}
-    </>
-  );
+      try {
+        const result = await sendTokenAndRedirect(token, redirectUrl);
+        router.push(result);
+      } catch (error) {
+        console.error("인증 실패:", error);
+      }
+    };
+
+    fetchData();
+  }, [token, redirectUrl]);
+
+  return <Suspense fallback={<p>로그인 처리 중...</p>}></Suspense>;
 }
--- src/components/button/loginButton.tsx
+++ src/components/button/loginButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useWebviewLogin } from "@/hooks/useWebviewLogin";
+// import { useWebviewLogin } from "@/hooks/useWebviewLogin";
 import { usePathname, useRouter } from "next/navigation";
 import style from "./loginButton.module.scss";
 
@@ -15,14 +15,14 @@ export function LoginButton({ size = "medium" }: LoginButtonProps) {
     const redirectUrl = encodeURIComponent(pathname);
     router.push(`signin/testPage?&redirect=${redirectUrl}`);
   };
-  const handleLoginSuccess = (accessToken: string) => {
-    const redirectUrl = encodeURIComponent(pathname);
-    router.push(
-      `signin/processing?token=${accessToken}&redirect=${redirectUrl}`
-    );
-  };
+  // const handleLoginSuccess = (accessToken: string) => {
+  //   const redirectUrl = encodeURIComponent(pathname);
+  //   router.push(
+  //     `signin/processing?token=${accessToken}&redirect=${redirectUrl}`
+  //   );
+  // };
 
-  const login = useWebviewLogin({ onLoginSuccess: handleLoginSuccess });
+  // const login = useWebviewLogin({ onLoginSuccess: handleLoginSuccess });
 
   return (
     <button onClick={loginTest} className={`${style.btn} ${style[size]}`}>
