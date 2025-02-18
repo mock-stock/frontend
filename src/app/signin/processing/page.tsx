ecf069af593d8b695acdf460fec6853e5be10447
suin <73993670+whl5105@users.noreply.github.com>
2025-02-18 21:11:37 +0900

    fix : useSearchParams를 Suspense로 감싸 프리렌더링 오류 해결
    - 데이터 가져오는 로직을 FetchDataComponent로 분리
    - 로딩 상태 처리를 위해 Suspense 추가하여 CSR 이탈 방지
    - 서버 핸들러 파일 삭제

--- src/app/signin/processing/page.tsx
+++ src/app/signin/processing/page.tsx
@@ -1,31 +1,17 @@
 "use client";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
+import { useEffect, useState, Suspense } from "react";
 
-const sendTokenAndRedirect = async (
-  accessToken: string,
-  redirectUrl: string
-) => {
-  const response = await axios.post(
-    "/api/auth/sendToken",
-    { accessToken },
-    { headers: { "Content-Type": "application/json" } }
-  );
-
-  if (response.data.success) {
-    return redirectUrl;
-  } else {
-    throw new Error(response.data.message || "인증 실패");
-  }
-};
-
-export default function ProgressPage() {
+const FetchDataComponent = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
   const token = searchParams.get("token");
   const redirectUrl = searchParams.get("redirect") || "/";
+
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,16 +20,36 @@ export default function ProgressPage() {
         return;
       }
 
-      try {
-        const result = await sendTokenAndRedirect(token, redirectUrl);
-        router.push(result);
-      } catch (error) {
-        console.error("인증 실패:", error);
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/sendToken`,
+        {
+          accessToken: token,
+        }
+      );
+
+      if (response.data.success) {
+        router.push(redirectUrl);
+      } else {
+        setErrorMessage(response.data.message || "인증 실패");
       }
+
+      setLoading(false);
     };
 
     fetchData();
   }, [token, redirectUrl]);
 
-  return <Suspense fallback={<p>로그인 처리 중...</p>}></Suspense>;
+  if (loading) {
+    return <p>로그인 처리 중...</p>;
+  }
+
+  return errorMessage ? <p>{errorMessage}</p> : null;
+};
+
+export default function ProgressPage() {
+  return (
+    <Suspense fallback={<p>로딩 중...</p>}>
+      <FetchDataComponent />
+    </Suspense>
+  );
 }
